1a6d8f06c9926c0799acdba0d7e9f38fa0ac368d
Harshitha P <115202188+Harshithapgorur@users.noreply.github.com>
2023-01-08 11:14:27 +0530

    Add files via upload

--- Olympic_game.sql
+++ Olympic_game.sql
@@ -1,1 +1,50 @@
-create table olympic_game(id int(50),game_type varchar(60));
+CREATE DATABASE jan07;
+
+use  jan07;
+
+CREATE TABLE olympic_game(id int,game_type varchar(100),player_name char(87),country char(100),no_of_teams int,no_of_players char(76),no_of_medals int,medal_type char(87),jersey_no int,winning_pize_amount int); 
+INSERT INTO olympic_game values(1,'athletics','player1','india',24,34,5,'gold',09,70000);
+INSERT INTO olympic_game values(2,'balkan_relay','player2','india',34.76,9,'silver',08,56789);
+INSERT INTO olympic_game values(3,'badmintor','player3','india',24,34,5,'gold',07,7900);
+INSERT INTO olympic_game values(4,'maharashtra','player4','india',34.76,9,'silver',08,58789);
+INSERT INTO olympic_game values(5,'dominant','player5','india',24,34,5,'gold',06,90000);
+INSERT INTO olympic_game values(6,'alpine_skiing','player6','india',34.76,9,'silver',25,6789);
+INSERT INTO olympic_game values(7,'archery','player7','india',24,34,5,'gold',56,56000);
+INSERT INTO olympic_game values(8,'artistic_swimming','player8','india',34.76,9,'silver',98,12789);
+INSERT INTO olympic_game values(9,'athletics','player9','india',24,34,5,'gold',94,98000);
+INSERT INTO olympic_game values(10,'baseball_softball','player10','india',34.76,9,'silver',21,545789);
+INSERT INTO olympic_game values(11,'basketball','player11','india',24,34,5,'gold',09,70000);
+INSERT INTO olympic_game values(12,'beach_handball','player12','india',34.76,9,'silver',08,56789);
+INSERT INTO olympic_game values(13,'beach_volleyball','player13','india',24,34,5,'gold',07,7900);
+INSERT INTO olympic_game values(14,'biathlon','player14','india',34.76,9,'silver',08,58789);
+INSERT INTO olympic_game values(15,'bobsleigh','player15','india',24,34,5,'gold',06,90000);
+INSERT INTO olympic_game values(16,'boxing','player16','india',34.76,9,'silver',25,6789);
+INSERT INTO olympic_game values(17,'breaking','player17','india',24,34,5,'gold',56,56000);
+INSERT INTO olympic_game values(18,'artistic_swimming','player18','india',34.76,9,'silver',98,12789);
+INSERT INTO olympic_game values(19,'athletics','player19','india',24,34,5,'gold',94,98000);
+INSERT INTO olympic_game values(20,'baseball_softball','player20','india',34,76,9,'silver',21,545789);
+INSERT INTO olympic_game values(21,'canoe_flatwater','player21','india',24,34,5,'gold',09,70000);
+INSERT INTO olympic_game values(22,'cross-counte=ry','player22','india',34.76,9,'silver',08,56789);
+INSERT INTO olympic_game values(23,'curling','player23','india',24,34,5,'gold',07,7900);
+INSERT INTO olympic_game values(24,'diving','player24','india',34.76,9,'silver',08,58789);
+INSERT INTO olympic_game values(25,'equestrian','player25','india',24,34,5,'gold',06,90000);
+INSERT INTO olympic_game values(26,'fencing','player26','india',34.76,9,'silver',25,6789);
+INSERT INTO olympic_game values(27,'figure','player27','india',24,34,5,'gold',56,56000);
+INSERT INTO olympic_game values(28,'freestyle_skiing','player28','india',34.76,9,'silver',98,12789);
+INSERT INTO olympic_game values(29,'futsal','player29','india',24,34,5,'gold',94,98000);
+INSERT INTO olympic_game values(30,'golf','player30','india',34.76,9,'silver',21,545789);
+INSERT INTO olympic_game values(31,'handball','player31','india',24,34,5,'gold',09,70000);
+INSERT INTO olympic_game values(32,'hockey','player32','india',34.76,9,'silver',08,56789);
+INSERT INTO olympic_game values(33,'ice_hokey','player33','india',24,34,5,'gold',07,7900);
+INSERT INTO olympic_game values(34,'judo','player34','india',34.76,9,'silver',08,58789);
+INSERT INTO olympic_game values(35,'karate','player35','india',24,34,5,'gold',06,90000);
+INSERT INTO olympic_game values(36,'luge','player36','india',34.76,9,'silver',25,6789);
+INSERT INTO olympic_game values(37,'rowing','player37','india',24,34,5,'gold',56,56000);
+INSERT INTO olympic_game values(38,'sailing','player38','india',34.76,9,'silver',98,12789);
+INSERT INTO olympic_game values(39,'tennis','player39','india',24,34,5,'gold',94,98000);
+INSERT INTO olympic_game values(40,'trampoline','player40','india',34.76,9,'silver',21,545789);
+
+
+
+
+
